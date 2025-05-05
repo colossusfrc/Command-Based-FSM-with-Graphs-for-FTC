0156404a8beafa43ddf4959fa3ba760291b51cf1
Colossus FRC <colossusfrc@gmail.com>
2025-05-05 17:02:05 -0300

    commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -10,7 +10,6 @@ public class Main extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         new RobotContainer(this);
-
         waitForStart();
         while (opModeIsActive()){
             CommandScheduler.getInstance().run(this);
